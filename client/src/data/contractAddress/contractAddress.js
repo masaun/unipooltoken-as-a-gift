c4b323dc9030ba63b75135417ef9c4dd9d9eab12
masaun <masanoriuno@gmail.com>
2020-05-20 09:07:05 +0900

    Add 'UniswapV2Factory' address into /data

--- client/src/data/contractAddress/contractAddress.js
+++ client/src/data/contractAddress/contractAddress.js
@@ -4,7 +4,12 @@ export const contractAddressList = {
             "BokkyPooBahsDateTimeContract": "0x2ac864D31b541267A821408A952E954B6b2AC9eA"
         },
         "Uniswap": {
-            "": "",
+            "UniswapV2Factory": "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",          
+        },
+    },
+    "Rinkeby": {
+        "Uniswap": {
+            "UniswapV2Factory": "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",          
         },
     }
 }
